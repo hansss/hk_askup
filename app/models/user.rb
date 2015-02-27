043149b56bad82cff7a8039a1b27647b6f60afd3
hansss <hanakim@college.harvard.edu>
2015-02-27 15:34:33 -0500

    added html button and filter for my vs all questions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
   has_many :questions
+  accepts_nested_attributes_for :questions
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, recoverable, and :omniauthable
   devise :database_authenticatable, :registerable,
          :rememberable, :trackable, :validatable
 
+  
   def full_name
     first_name + " " + last_name
   end
